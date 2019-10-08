adc6a4f184a7c2c7d16e0f0fbd4d73007c7512c4
lburatti <lburatti89@gmail.com>
2019-10-08 18:44:20 -0300

    Adicionando arquivos da pré entrega da Sprint 3

---
+++ Sprint 3/inc/header.php
@@ -0,0 +1,24 @@
+<?php include_once ('inc/head.php'); ?>
+<header class="container-fluid p-0 m-0">
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand m-0 p-0" href="home.html">
+            <img src="img/logo.png" alt="Logo" height="65" width="90">
+        </a>
+        <div class="d-flex flex-row ml-auto">
+            <div class="dropdown mx-1">
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" style="color: #EB0045" href="#">Serviços</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: #EB0045" href="cadastro.html">Parceiros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: #EB0045" href="cadastro.html">Cadastro</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: #EB0045" href="login.html">Login</a>
+                    </li>
+                </ul>
+            </div>
+</header>
