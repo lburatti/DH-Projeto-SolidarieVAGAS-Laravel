4adcfc92fdc0e145e7e7a07ac12911b24dbb93f5
HigorMatheus <hmoliveira83@gmail.com>
2019-12-08 13:21:00 -0300

    login e logout funcionando

--- Sprint 5 - Laravel/resources/views/layouts/app.blade.php
+++ Sprint 5 - Laravel/resources/views/layouts/app.blade.php
@@ -15,6 +15,122 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div id="app">
+    {{-- <header class="container-fluid p-0 m-0"> --}}
+        <nav class="navbar fixed-top navbar-expand-lg navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span ><img src="/img/logo.png" alt="Logo" height="60" width="90" class="mr-3"></span>
+                </button> 
+                    {{-- <button class="navbar-brand my-auto" href="{{ sroute('welcome') }}">
+                        <img src="/img/logo.png" alt="Logo" height="60" width="90" class="mr-3">
+                    </button> --}}
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                    
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('busca.index') }}" title="Busca">Buscas</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('servicos') }}">Serviços</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('cadastro') }}">Cadastros</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " style="color:#FF6347" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            {{-- @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:#FF6347" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif --}}
+                        @else
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('adm.usuarios.index') }}">Usuários(1)</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('adm.niveis-acesso.index') }}">Níveis de acesso(1)</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('adm.servicos.index') }}">Serviços(1)</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('adm.meu-cadastro') }}">Meu cadastro(2,3)</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('adm.profissionais.index') }}">Profissionais(2)</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('adm.pedidos-recebidos.index') }}">Pedidos Recebidos(2)</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('adm.pedidos-enviados.index') }}">Pedidos enviados(3)</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" style="color:#FF6347" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
+
+
+{{-- <!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- ADOBE -->
     <link rel="stylesheet" href="https://use.typekit.net/bbn7xbn.css">
 
@@ -29,6 +145,144 @@
 </head>
 <body>
     <div id="app">
+        
+        <!-- Authentication Links -->
+        @guest
+        <!-- HEADER não logado-->
+        <header class="container-fluid p-0 m-0">
+                <nav class="navbar fixed-top navbar-expand-lg">
+                    <a class="navbar-brand my-auto" href="{{ sroute('welcome') }}">
+                        <img src="/img/logo.png" alt="Logo" height="60" width="90" class="mr-3">
+                        SolidarieVAGAS
+                    </a>
+                    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('busca.index') }}" title="Busca">Buscas</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('servicos') }}">Serviços</a>
+                            </li>
+                            <li class="nav-item mx-1">
+                                <a class="nav-link" style="color:#FF6347" href="{{ route('cadastro') }}">Cadastros</a>
+                            </li>
+                            <button type="button" class="btn btn-light" data-toggle="modal" id="modalLogin" data-target="#exampleModal">
+                                Login</button>
+                        </ul>
+                    </div>
+                </nav>
+                 <!-- Modal  login -->
+                <div class="modal fade aling-intens-center" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="login">Login</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="form-group col-md-12" id="texto-login-cadastro">
+                                        <label for="email">E-mail </label>
+                                        <input type="email" name="email" class="form-control login-cadastro-input" id="email" aria-describedby="emailHelp" placeholder="email@email.com.br">
+                                        <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+                                    </div>
+                                    <div class="form-group col-md-12" id="texto-login-cadastro">
+                                        <label for="senha">Senha</label>
+                                        <input type="password" name="senha" class="form-control login-cadastro-input" id="senha" placeholder="Digite uma senha">
+                                    </div>
+                                    <div class="form-group col-md-12 ml-4" id="texto-login-cadastro form-check">
+                                        <input type="checkbox" class="form-check-input login-casastro-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Manter conectado</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary ml-3" id="botao-login" href="adm_usuarios.php">Entrar</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+    
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </header>
+        <div></div>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+        @endif
+
+
+
+    @else
+             <!-- HEADER Logado -->
+        <header class="container-fluid p-0 m-0" id="adm">
+            <nav class="navbar fixed-top navbar-expand-lg">
+                <a class="navbar-brand my-auto" href="home.php">
+                    <img src="/img/logo.png" alt="Logo" height="60" width="90" class="mr-3">
+                    SolidarieVAGAS
+                </a>
+                <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mx-1">
+                            <a class="nav-link" style="color:#FF6347" href="{{ route('adm.usuarios.index') }}">Usuários(1)</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a class="nav-link" style="color:#FF6347" href="{{ route('adm.niveis-acesso.index') }}">Níveis de acesso(1)</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a class="nav-link" style="color:#FF6347" href="{{ route('adm.servicos.index') }}">Serviços(1)</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a class="nav-link" style="color:#FF6347" href="{{ route('adm.meu-cadastro') }}">Meu cadastro(2,3)</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a class="nav-link" style="color:#FF6347" href="{{ route('adm.profissionais.index') }}">Profissionais(2)</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a class="nav-link" style="color:#FF6347" href="{{ route('adm.pedidos-recebidos.index') }}">Pedidos Recebidos(2)</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a class="nav-link" style="color:#FF6347" href="{{ route('adm.pedidos-enviados.index') }}">Pedidos enviados(3)</a>
+                        </li>
+                        <button type="submit" class="btn btn-light" id="botao-sair">
+                            <a class="text-white text-decoration-none" href="home.php">Sair</a>
+                        </button>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        <div>
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+        </li>
+    </div>
+    @endguest
         <!-- HEADER -->
         <header class="container-fluid p-0 m-0">
             <nav class="navbar fixed-top navbar-expand-lg">
@@ -115,4 +369,4 @@
         </footer>
     </div>
 </body>
-</html>
+</html>  --}}
